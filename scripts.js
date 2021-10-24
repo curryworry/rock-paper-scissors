ebf76b03518491a531c46981c1cccdd94e662e54
Ash Sundaram <ashwinrsundaram@gmail.com>
2021-10-24 16:48:33 +1300

    Built DOM events, game logic, history log, and made UI changes.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
 const gameController = document.querySelector('#game-controller');
-
+const choiceOptions = document.querySelectorAll('img');
+const resultsDiv = document.querySelector('#history');
+const containerDiv = document.querySelector('#container');
+console.log(choiceOptions);
 app();
 
 function app(){
@@ -10,9 +13,9 @@ function app(){
 }
 
 function gameIntro(e){
-    console.log(e);
     e.target.remove();
     gameController.textContent = "Choose Rock, Paper, or Scissors! First to 5 wins!";
+    choiceOptions.forEach((choice)=>{choice.addEventListener('click',game)});
 }
 
 function computerPlay(){
@@ -23,19 +26,33 @@ function computerPlay(){
     return computerSelection.toUpperCase();
 }
 
-function playerPlay(){
+function playerPlay(e){
     // Player Enters a choice
-    console.log("Enter a choice: ROCK/PAPER/SCISSORS?")
-    let playerInput = window.prompt("Enter a choice: ROCK/PAPER/SCISSORS");
-    let playerSelection = playerInput.toUpperCase();
-    if(validatePlayerChoice(playerSelection)){
-        console.log(`Player's selection is ${playerSelection}`);
-        return playerSelection;
+    //console.log("Enter a choice: ROCK/PAPER/SCISSORS?")
+    console.log(e);
+    let playerSelection;
+    if(e.target.id=="rock"){
+     playerSelection = "ROCK";
+    }
+    else if(e.target.id=="paper"){
+     playerSelection = "PAPER";
+    }
+    else if(e.target.id =="scissors"){
+     playerSelection = "SCISSORS";
     }
     else{
-        console.log("Invalid Choice: Choose either ROCK/PAPER/SCISSORS");
-        playerPlay();
+        console.log("Invalid Choice");  
     }
+    return playerSelection;
+    //let playerSelection = playerInput.toUpperCase();
+    // if(validatePlayerChoice(playerSelection)){
+    //     console.log(`Player's selection is ${playerSelection}`);
+    //     return playerSelection;
+    // }
+    // else{
+    //     console.log("Invalid Choice: Choose either ROCK/PAPER/SCISSORS");
+    //     playerPlay(e);
+    // }
 }
 
 function validatePlayerChoice(playerSelection){
@@ -54,45 +71,81 @@ function playRound(computerSelection,playerSelection){
     //One round of RPS is played and winner of the round is decided
     if(computerSelection=="ROCK"&&playerSelection=="PAPER"){
         console.log("Player wins round!");
+        updateHistory(computerSelection,playerSelection,"Player");
         playerScore++;
     }
     else if(computerSelection=="PAPER"&&playerSelection=="SCISSORS"){
         console.log("Player wins round!");
+        updateHistory(computerSelection,playerSelection,"Player");
         playerScore++;
     }
     else if(computerSelection=="SCISSORS"&&playerSelection=="ROCK"){
         console.log("Player wins round!");
+        updateHistory(computerSelection,playerSelection,"Player");
         playerScore++;        
     }
     else if(computerSelection==playerSelection){
         console.log("Round drawn!");
+        updateHistory(computerSelection,playerSelection,"Neither");
     }
     else{
         console.log("Computer wins round!");
+        updateHistory(computerSelection,playerSelection,"Computer");
         computerScore++;
     }
-    scoreUpdate();
+    scoreUpdate(computerSelection,playerSelection);
 }
 
-function game(){
-    //five rounds are played and winner of the game is decided
-    console.log("Welcome to ROCK, PAPER, SCISSORS!"); 
-    console.log("You will now play a 'First to 5' game of Rock, Paper, Scissors.");
-    console.log("The first player to reach five points wins.");
-    while(playerScore<5&&computerScore<5){
-        let playerSelection = playerPlay();
+function game(e){
+    //rounds are played until player/computer reaches 5
+    if(playerScore<5&&computerScore<5){
+        let playerSelection = playerPlay(e);
         let computerSelection = computerPlay();
         playRound(computerSelection,playerSelection);
     }
-    if(playerScore==5){
-        console.log("PLAYER WINS GAME!");
-    }
-    else{
-    console.log("COMPUTER WINS GAME!");
-    }
 }
 
-function scoreUpdate(){
+function scoreUpdate(computerSelection,playerSelection){
     //give player an update of score so far
     console.log(`Computer's current score is ${computerScore}, and Player's current score is ${playerScore}`);
+    let leader = computerScore>playerScore?"Computer":playerScore>computerScore?"Player":"Neither";
+    gameController.textContent = `Player's score is ${playerScore}. Computer's score is ${computerScore}. ${leader} Leads!`;
+    if(playerScore==5){
+        gameController.textContent = "PLAYER WINS!";
+        const replayButton = document.createElement('button');
+        replayButton.classList.add('btn');
+        replayButton.setAttribute('id','replay');
+        replayButton.textContent = "Click to play again!";
+        containerDiv.insertBefore(replayButton,resultsDiv);
+        const replay = document.querySelector('#replay');
+        replay.addEventListener('click',restartGame);
+    }
+    else if(computerScore==5){
+        gameController.textContent = "COMPUTER WINS!";
+        const replayButton = document.createElement('button');
+        replayButton.classList.add('btn');
+        replayButton.setAttribute('id','replay');
+        replayButton.textContent = "Click to play again!";
+        containerDiv.insertBefore(replayButton,resultsDiv);
+        const replay = document.querySelector('#replay');
+        replay.addEventListener('click',restartGame);
+    }
+    
+}
+
+function updateHistory(computerSelection,playerSelection,winner){
+    const roundResult = document.createElement('p');
+    roundResult.textContent = `Player chose ${playerSelection}. Computer chose ${computerSelection}. ${winner} won round.`;
+    roundResult.classList.add('round-results');
+    resultsDiv.classList.remove('hidden-item');
+    resultsDiv.appendChild(roundResult);
+}
+
+function restartGame(e){
+    playerScore=0;
+    computerScore=0;
+    const resultItems = document.querySelectorAll('.round-results');
+    resultItems.forEach((resultItem)=>resultItem.remove());
+    resultsDiv.classList.add('hidden-item');
+    gameIntro(e);
 }
